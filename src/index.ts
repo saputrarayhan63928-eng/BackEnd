44949b24d8ce9f36c2948dc9c87a3716b83ebd9e
rayhan <rayhangemming8@gmail.com>
2026-02-04 14:19:42 +0700

    node.js commit

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import http from "node:http";
+import { hello } from "#hello";
+import moment from "moment";
+// import { url } from "node:inspector";
+
+const requestListener = (
+  req: http.IncomingMessage,
+  res: http.ServerResponse,
+) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  // res.write(`Hello, World! Waktu Saat Ini: ${moment().format('YYYY-MM-DD HH:mm:ss')}`)
+  // res.end(JSON.stringify({
+  //     message: hello(),
+  //     time: moment().format('YYYY-MM-DD HH:mm:ss'),
+  //     url: req.url
+  // }))
+
+  if (req.url === "/") {
+    res.end(
+      JSON.stringify({
+        message: hello(),
+        time: moment().format("YYYY-MM-DD HH:mm:ss"),
+        url: req.url,
+      }),
+    );
+  } else if (req.url === "/time") {
+    res.end(
+      JSON.stringify({
+        message: "You Are  accessing endPoint /time",
+        time: moment().format("YYYY-MM-DD HH:mm:ss"),
+        url: req.url,
+      }),
+    );
+  } else {
+    res.statusCode = 404;
+    res.write(
+      JSON.stringify({
+        message: "You Are Accessing endpoind that not found",
+        url: req.url,
+      }),
+    );
+    res.end();
+  }
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000/`);
+});
