0f7c663e99b9f0808cf7b0258deb105927fb55b4
rayhan <rayhangemming8@gmail.com>
2026-02-05 13:47:02 +0700

    commot node.js

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,128 @@
-// import http from "node:http";
-// import { hello } from "#hello";
-// import moment from "moment";
-// // import { url } from "node:inspector";
+import express, {type Request, type Response } from 'express';
+import dotenv from 'dotenv'
 
-// const requestListener = (
-//   req: http.IncomingMessage,
-//   res: http.ServerResponse,
-// ) => {
-//   res.statusCode = 200;
-//   res.setHeader("Content-Type", "text/plain");
-//   // res.write(`Hello, World! Waktu Saat Ini: ${moment().format('YYYY-MM-DD HH:mm:ss')}`)
-//   // res.end(JSON.stringify({
-//   //     message: hello(),
-//   //     time: moment().format('YYYY-MM-DD HH:mm:ss'),
-//   //     url: req.url
-//   // }))
 
-//   if (req.url === "/") {
-//     res.end(
-//       JSON.stringify({
-//         message: hello(),
-//         time: moment().format("YYYY-MM-DD HH:mm:ss"),
-//         url: req.url,
-//       }),
-//     );
-//   } else if (req.url === "/time") {
-//     res.end(
-//       JSON.stringify({
-//         message: "You Are  accessing endPoint /time",
-//         time: moment().format("YYYY-MM-DD HH:mm:ss"),
-//         url: req.url,
-//       }),
-//     );
-//   } else {
-//     res.statusCode = 404;
-//     res.write(
-//       JSON.stringify({
-//         message: "You Are Accessing endpoind that not found",
-//         url: req.url,
-//       }),
-//     );
-//     res.end();
-//   }
-// };
+dotenv.config()
 
-// const server = http.createServer(requestListener);
+const app = express()
+const PORT = process.env.PORT || 3000
 
-// server.listen(3000, () => {
-//   console.log(`Server running at http://localhost:3000/`);
-// });
+app.use(express.json())
+app.use(express.urlencoded ({extended: true}))
+
+let products = [
+  { id: 1, nama: "Laptop Gaming", deskripsi: "Intel i7, RTX 3060", harga: 15000000 },
+  { id: 2, nama: "Keyboard Mekanikal", deskripsi: "Blue Switch, RGB", harga: 800000 },
+  { id: 3, nama: "Mouse Wireless", deskripsi: "Ergonomic, Silent Click", harga: 300000 }
+];
+
+app.get('/', (_req: Request, res: Response) =>{
+    res.json({
+         message: "Selamat Datang di  Api Ecomers",
+         hari: 3,
+         status: "server hidup!"
+    })
+})
+
+app.get('/api/products', (_req: Request, res: Response) => {
+  res.json({
+    success: true,
+    jumlah: products.length,
+    data: products
+  });
+});
+
+app.get('/api/products/:id', (req: Request, res: Response) =>{
+    const id = parseInt(req.params.id as string)
+    const product = products.find(p => p.id === id)
+
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "Product Tidak di Temukan"
+        })
+    }
+
+    res.json({
+        success:true,
+        data: product
+    })
+})
+
+app.get('/api/search', (req: Request, res: Response) => {
+    const {name, max_price} = req.query
+
+    let result = products
+
+    if(name) {
+        result = result.filter(p => p.nama.toLocaleLowerCase().includes((name as string).toLocaleLowerCase())
+    )
+    }
+
+    if(max_price){
+        result = result.filter(p => p.harga <= Number(max_price))
+    }
+
+    res.json({
+        success:true,
+        filtered_result: result
+    })
+})
+
+app.post('/api/products', (req:Request,res:Response) => {
+    const {nama, deskripsi, harga} = req.body
+
+    const newProduct = {
+        id: products.length + 1,
+        nama,
+        deskripsi,
+        harga: Number(harga)
+    }
+
+    products.push(newProduct)
+
+    res.status(201).json({
+        success: true,
+        message: "Produk Berhasil di Tambahkan",
+        data: newProduct
+    })
+})
+
+app.put('/api/products/:id', (req:Request,res:Response) =>{
+    const id = parseInt(req.params.id as string)
+    const index = products.findIndex(p => p.id === id)
+
+    if (index === -1){
+        return res.status(404).json({success: false, message:"Product Tidak Ada"})  
+    }
+
+    products[index] = {...products[index],...req.body}
+
+    res.json({
+    success: true,
+    message: "Produk berhasil diupdate",
+    data: products[index]
+    })
+})
+
+app.delete('/api/products/:id', (req:Request,res:Response) => {
+    const id = parseInt(req.params.id as string)
+    const index = products.findIndex(p => p.id === id)
+
+    if(index === -1){
+        return res.status(404).json({ success: false, message: "Produk tidak ada" })
+    }
+
+    const deleted = products.splice(index,1)
+
+    res.json({
+    success: true,
+    message: "Produk berhasil dihapus",
+    data: deleted[0]
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server jalan → http://localhost:${PORT}`);
+  console.log(`Coba buka semua route di atas pakai Postman!`);
+});
